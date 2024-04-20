05935cabf0bc8b7c37ab16eff6299662acce4c66
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-04-20 22:27:55 +0530

    added delete controller

--- src/routes/v1/user/index.js
+++ src/routes/v1/user/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authMiddleware = require('../../../middlewares/auth-middleware');
-const { getUserController , updateUserController, changeUserPasswordController} = require('../../../controllers/user-controller');
+const { getUserController , updateUserController, changeUserPasswordController, deleteUserController} = require('../../../controllers/user-controller');
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get('/user/getUser', authMiddleware, getUserController);
 router.put('/user/updateUser', authMiddleware, updateUserController );
 
 router.post('/user/changePassword', authMiddleware, changeUserPasswordController);
+
+router.delete('/user/deleteUser', authMiddleware, deleteUserController);
 
 module.exports = router;
