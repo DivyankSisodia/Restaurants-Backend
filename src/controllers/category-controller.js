f312592212f8832e04e9ef1ca5115e261c72358f
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-04-23 08:51:49 +0530

    minor changes

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,4 +1,4 @@
-const Category = require('../models/category');
+const Category = require('../models/category.model');
 const {color} = require('colors');
 const uploadOnCloudinary = require('../utils/cloudinary');
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         // file has been uploaded successfully
         fs.unlinkSync(localFilePath)
         return response;
-
+                                                                            
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
