6a27376f5dac3638b32ccf3dd5b8a9b26b760102
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-04-23 12:55:38 +0530

    created routes for search api and get food apis

--- src/routes/v1/food/index.js
+++ src/routes/v1/food/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const createFoodController = require('../../../controllers/food-controller');
+const { createFoodController, getAllFoodController, getSingleFoodController, searchFoodController } = require('../../../controllers/food-controller');
 const upload = require('../../../middlewares/multer-middleware');
 
 const authMiddleware = require('../../../middlewares/auth-middleware');
 
+// create a food
 router.post('/food/create', upload.single('image'), authMiddleware, createFoodController);
+
+// get all foods
+router.get('/food/all', getAllFoodController);
+
+// get food by id
+router.get('/food/get/:id', getSingleFoodController);
+
+router.get('/food/search/:key', searchFoodController);
 
 module.exports = router;
